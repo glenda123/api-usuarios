c75ed48f25551fe834dd7531b85bdd4264b30c65
Glenda Guerrero <glendaguerrerof@gmail.com>
2020-07-16 15:12:54 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      formulario:{}
     };
   }
 
@@ -21,13 +22,13 @@ class App extends React.Component {
 
   addUser = event => {
     event.preventDefault();
-    //Agregar un post
+    
     fetch("https://academlo-api-users.herokuapp.com/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=UTF-8"
       },
-      body: JSON.stringify()
+      body: JSON.stringify(this.state.formulario)
     })
       .then(response => response.json())
       .then(results => console.log(results))
@@ -36,27 +37,39 @@ class App extends React.Component {
 
 
   handleInput = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({
+      formulario: {
+        ...this.state.formulario,[event.target.name]: event.target.value
+      }
+    });
   };
 
   render(){
     return (
       <div>
         <form onInput={this.handleInput} onSubmit={this.addUser}>
-          <input name="name" type="text" placeholder="nombre del usuario" />
-          <input name="lastname" type="text" placeholder="apellido" />
-          <input name="email" type="text" placeholder="correo" />
-          <input name="password" type="password" placeholder="contraseña" />
-          <input type="submit" />
+          <br></br>
+          <div className="nombre">Nombre :</div> &nbsp; &nbsp;
+          <input className="name" name="name" type="text" placeholder="nombre del usuario" /> <br></br><br></br>
+          <div className="apellido">Apellido :</div> &nbsp; &nbsp;
+          <input  className="lastname" name="lastname" type="text" placeholder="apellido" /><br></br><br></br>
+          <div className="correo">Correo :</div> &nbsp; &nbsp;
+          <input className="email" name="email" type="text" placeholder="correo" /> <br></br><br></br>
+          <div className="contrasenia">Nombre :</div> &nbsp; &nbsp;
+          <input className="password" name="password" type="password" placeholder="contraseña" /> <br></br><br></br>
+          <input type="submit" /><br></br><br></br>
         </form>
         <div>
           {this.state.users.map(user => {
             return (
-              <div>
-                <div>{user.name}</div>
-                <div>{user.lastname}</div>
-                <div>{user.email}</div>
-                <div>{user.password}</div>
+              <div className="grid-container">
+                  <div className="tarjeta">
+                       <br></br>
+                        <div>{user.name}</div> <br></br>
+                        <div>{user.lastname}</div><br></br>
+                        <div>{user.email}</div> <br></br>
+                        <div>{user.password}</div><br></br>
+                  </div>
               </div>
             );
           })}
